e49ce763f8e5687895ea72bdcf48295fae1dab5d
Sewmini De Silva <166793173+sewminiDeSilva@users.noreply.github.com>
2025-09-16 23:56:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -176,11 +176,14 @@ app.get('/api/recommendations', async (req, res) => {
 
 
 // Serve frontend in production
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
+  });
+}
+
 
 
 // Init function
