b9df440b610fbe537f994cafc090ef0d23c77a5b
Sewmini De Silva <166793173+sewminiDeSilva@users.noreply.github.com>
2025-09-16 23:11:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -172,6 +172,16 @@ app.get('/api/recommendations', async (req, res) => {
   }
 });
 
+
+// Serve frontend in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+  });
+}
+
 // Init function
 async function init() {
   try {
